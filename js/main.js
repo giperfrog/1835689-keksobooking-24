13beac8b95d31fde5659970147f12101b17cd6df
Ольга Кашина <giperfrog@gmail.com>
2021-09-27 19:58:52 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-function getRandomInt(min, max) { //Возвращает случайное целое число из переданного диапазона включительно, взято с MDN Web Docs.
+const getRandomInt = (min, max) => { //Возвращает случайное целое число из переданного диапазона включительно, взято с MDN Web Docs.
   if (max <= min) {
-    return 'Ошибка! Верхний диапазон должен быть больше нижнего!'; //Проверка верхнего предела.
+    throw 'Ошибка! Верхний диапазон должен быть больше нижнего!'; //Проверка верхнего предела.
   }
 
   if (min < 0 || max < 0) {
-    return 'Проверьте диапазон!'; //Проверка на положительный диапазон.
+    throw 'Диапазон должен быть положительным!'; //Проверка на положительный диапазон.
   }
 
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandomInt(0, 100);
 
-function getRandomFloat(min, max, decimal) { //Возвращает случайное число с плавающей точкой из переданного диапазона включительно.
+const getRandomFloat = (min, max, decimal) => { //Возвращает случайное число с плавающей точкой из переданного диапазона включительно.
   if (max <= min) {
-    return 'Ошибка! Верхний диапазон должен быть больше нижнего!';
+    throw 'Ошибка! Верхний диапазон должен быть больше нижнего!';
   }
 
-  if (min < 0 || max < 0) {
-    return 'Проверьте диапазон!';
+  if (min < 0 || max < 0 || decimal < 0) {
+    throw 'Диапазон должен быть положительным!';
   }
 
-  min = min.toFixed(decimal);
-  max = max.toFixed(decimal);
-  return Math.random() * (max - min + 1) + min;
-}
+  return Number((Math.random() * (max - min) + min).toFixed(decimal));
+};
 
 getRandomFloat(0, 100.333, 2);
