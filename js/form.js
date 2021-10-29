62d4d5e518f61bdf7d3d013e5ee8840d7248b3c9
Ольга Кашина <giperfrog@gmail.com>
2021-10-29 13:54:18 +0300

    Валидация формы

--- js/form.js
+++ js/form.js
@@ -32,3 +32,37 @@ const makePageActive = () => {
 
 makePageInactive();
 makePageActive();
+
+//Подсказка на короткое и длинное значение.
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const offerTitleInput = document.querySelector('#title');
+offerTitleInput.addEventListener('input', () => {
+  const titleLength = offerTitleInput.value.length;
+
+  if (titleLength < MIN_TITLE_LENGTH) {
+    offerTitleInput.setCustomValidity(`Ещё ${MIN_TITLE_LENGTH - titleLength} симв.`);
+  } else if (titleLength > MAX_TITLE_LENGTH) {
+    offerTitleInput.setCustomValidity(`Удалите лишние ${titleLength - MAX_TITLE_LENGTH} симв.`);
+  } else {
+    offerTitleInput.setCustomValidity('');
+  }
+  offerTitleInput.reportValidity();
+});
+
+// Проверка соответствия количества комнат количеству гостей.
+const roomNumber = document.querySelector('#room_number');
+const guestNumber = document.querySelector('#capacity');
+const roomNumberCollection = roomNumber.options;
+const guestNumberCollection = guestNumber.options;
+
+guestNumber.addEventListener('change', () => {
+  if (roomNumberCollection[0] === guestNumberCollection[2]) {
+    guestNumber.setCustomValidity('');
+  } else {
+    guestNumber.setCustomValidity(`Можно выбрать только "${guestNumberCollection[2].label}".`);
+  }
+  guestNumber.reportValidity();
+});
+
